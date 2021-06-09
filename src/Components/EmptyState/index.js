fa5692b7637526cd613af10c6bb1871fc22f5998
sagarchoudhary96 <sagar.choudhary96@gmail.com>
2021-06-10 01:48:23 +0530

    Add EmptyState Component

---
+++ src/Components/EmptyState/index.js
@@ -0,0 +1,45 @@
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import Paper from "@material-ui/core/Paper";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import LibraryBooksIcon from "@material-ui/icons/LibraryBooks";
+
+const useStyles = makeStyles({
+  wrapper: {
+    height: "100%",
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+const EmptyState = ({
+  title,
+  subtitle,
+  icon,
+  titleVariant = "h5",
+  subtitleVariant = "subtitle2",
+}) => {
+  const classes = useStyles();
+  return (
+    <Paper className={classes.wrapper}>
+      {icon ? icon : <LibraryBooksIcon />}
+      <Box
+        pt={1}
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+      >
+        {title && <Typography variant={titleVariant}>{title}</Typography>}
+        {subtitle && (
+          <Typography variant={subtitleVariant}>{subtitle}</Typography>
+        )}
+      </Box>
+    </Paper>
+  );
+};
+
+export default EmptyState;
--- src/utils/constants/common.js
+++ src/utils/constants/common.js
@@ -6,6 +6,8 @@ export const DRAWER_WIDTH = 240;
 // These can be later on move to separate file making it easier to do localisation for our app
 // in case we have functionality to support different languages
 export const DEFAULT_STRINGS = {
+  WELCOME_MESSAGE_TITLE: "Welcome to SQL QUERY EDITOR",
+  WELCOME_MESSAGE_SUBTITLE: "To get started, Enter and Run a Query",
   APP_TITLE: "SQL EDITOR",
   NO_TABLES_EXIST: "No table exists",
   IMPORT_NEW_DATA_MESSAGE: "Please import data to the Editor",
